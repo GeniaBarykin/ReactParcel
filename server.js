d1080a2a542cd4017a08c1646a71956733e78d21
GeniaBarykin <467232@student.saxion.nl>
2019-10-28 15:00:32 +0100

    Tests fixed but they do not work in ci/cd

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ app.use(function(req,rsp,next){
 //Include our rest api
 app.use(require('./rest-api/api-index'));
 
-var port = process.env.PORT || 5000;
+var port = 5000;
 app.listen(port);
 console.log('server started at port: ' + port);
